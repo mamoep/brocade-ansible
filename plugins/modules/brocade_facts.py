224981cf32d09a9efae0f3fceff0bb6e68d7fe91
mamoep <26142342+mamoep@users.noreply.github.com>
2023-04-13 11:14:48 +0200

    revert namespace change

--- plugins/modules/brocade_facts.py
+++ plugins/modules/brocade_facts.py
@@ -182,11 +182,11 @@ Brocade Fibre Channel gather FOS facts
 """
 
 
-from ansible.module_utils.brocade_connection import login, logout, exit_after_login
-from ansible.module_utils.brocade_zoning import defined_get, effective_get, to_human_zoning
-from ansible.module_utils.brocade_objects import singleton_get, list_get, to_human_singleton, to_human_list, get_moduleName
-from ansible.module_utils.brocade_yang import str_to_yang
-from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.mamoep.brocade_fos.plugins.module_utils.brocade_connection import login, logout, exit_after_login
+from ansible_collections.mamoep.brocade_fos.plugins.module_utils.brocade_zoning import defined_get, effective_get, to_human_zoning
+from ansible_collections.mamoep.brocade_fos.plugins.module_utils.brocade_objects import singleton_get, list_get, to_human_singleton, to_human_list, get_moduleName
+from ansible_collections.mamoep.brocade_fos.plugins.module_utils.brocade_yang import str_to_yang
+from ansible_collections.mamoep.brocade_fos.plugins.module_utils.basic import AnsibleModule
 
 
 valid_areas = [
